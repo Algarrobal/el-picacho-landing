2238a7fa440b5f35579e3a448724a57b647f8117
Algarrobal <martin.bascon@alumni.ie.edu>
2025-05-14 14:24:47 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ElPicachoLanding from "./App";
+import AppRouter from "./AppRouter";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ElPicachoLanding />
+    <AppRouter />
   </React.StrictMode>
 );
