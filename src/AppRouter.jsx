bca1ef6ea1e21d6e54f37118e13f71cfdf287992
Algarrobal <martin.bascon@alumni.ie.edu>
2025-05-14 14:36:22 -0400

    "Reintroduce Picacheada import (route not active)"

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ElPicachoLanding from "./App";
+import Picacheada from "./Picacheada"; // ✅ Step 1: Reintroduce the import
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ElPicachoLanding />} />
+        {/* 👇 Don't re-add the route to /picacheada just yet */}
       </Routes>
     </BrowserRouter>
   );
