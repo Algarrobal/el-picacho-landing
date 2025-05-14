fd216e28e442cedb004c40a325ccbd983aeaf312
Algarrobal <martin.bascon@alumni.ie.edu>
2025-05-14 10:14:32 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <React.StrictMode>
     <AppRouter />
-  </BrowserRouter>
+  </React.StrictMode>
 );
