bb6933772cb62457b5f77f4c6cb4be3fe9f4319d
Algarrobal <martin.bascon@alumni.ie.edu>
2025-05-14 10:23:10 -0400

    Update AppRouter.jsx

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,16 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ElPicachoLanding from "./App";
-import Expresiones from "./Expresiones";
 import Picacheada from "./Picacheada";
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ElPicachoLanding />} />
-        <Route path="/expresiones" element={<Expresiones />} />
-        <Route path="/picacheada" element={<Picacheada />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<ElPicachoLanding />} />
+      <Route path="/picacheada" element={<Picacheada />} />
+    </Routes>
   );
 }
