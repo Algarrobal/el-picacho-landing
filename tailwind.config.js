2bbc526f468d7b9b5d482981114de77ca2381439
Algarrobal <martin.bascon@alumni.ie.edu>
2025-05-16 18:09:09 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        tap: "44px", // Adds utility like min-h-tap, h-tap, etc.
+      },
+    },
   },
   plugins: [],
-}
+};
